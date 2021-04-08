6f2099725f8c9e70cb462759bd80a29708d9bad2
Malyikanima Jackson <malyikaj@gmail.com>
2021-04-08 17:33:44 -0400

    1st commit

---
+++ forloops.js
@@ -0,0 +1,37 @@
+
+// for(let counter = 1; counter <= 100; counter++)
+// {
+//     var num = 5 * counter;
+//     console.log(`${num}`);
+// }
+// console.log(`The other message.`);
+
+
+
+// Exercise # 1: Add the first 20 numbers of counter (from 1-20). Counter increments by 1. Display the sum at the end.
+
+
+
+// let counter;
+// let sum = 0;
+// sum += counter
+
+// for(let counter = 1; counter <= 20; counter ++) {
+//     console.log(`${sum}`);
+// }
+
+
+
+// Exercise # 2 : Start and end values of the loop should be determined by user. The loops increments by 1, so the start value should be lower than the end value. Display only even numbers.
+let start = 1;
+let end = 40;
+
+for(let counter = start; counter<= end; counter++) {
+    if (counter%2 == 1 && counter%2 == 1) {
+        console.log(counter);
+    }
+}
+
+
+
+
